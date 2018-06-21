21c1a1c2adbae41a2b9e165f29dd56e266a2606e
Joshua Keating <himself@joshs.space>
2018-06-22 00:44:51 +1000

    MM-17: Created empty component for Job Runner

---
+++ src/JobRunner/index.js
@@ -0,0 +1,20 @@
+// @flow
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+type Props = {};
+
+class JobRunner extends Component<Props> {
+  componentDidMount() {}
+
+  render() {
+    return null;
+  }
+}
+
+const mapDispatchToProps = {};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(JobRunner);
