e2acc2dbb9f5f3335eb7795ab8751c0fbc3a8baf
jvke <hi@jvke.co>
2018-06-22 14:29:13 +1000

    Prelim 'get' request runner

--- src/shared/actions/jobRunners.js
+++ src/shared/actions/jobRunners.js
@@ -2,6 +2,23 @@
 import type { JobRequest } from '../../types';
 import type { Action, Thunk } from './types';
 
-export const httpGet = (data: JobRequest): Thunk => {
-  // request
+import axios from 'axios';
+
+export const get = (data: JobRequest): Thunk => {
+  const {
+    endpoint_address,
+    endpoint_port = '8080',
+    endpoint_additional_params
+  } = data;
+  const uri = `${endpoint_address}:${endpoint_port}/${endpoint_additional_params}`;
+  axios
+    .get(uri)
+    .then(console.info)
+    .catch(console.error);
 };
+
+export const post = (data: JobRequest): Thunk => {};
+
+export const traceroute = (data: JobRequest): Thunk => {};
+
+export const ping = (data: JobRequest): Thunk => {};
