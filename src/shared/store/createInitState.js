679894b31de1af5fb7a906fe5cec462cd9de47f1
Joshua Keating <himself@joshs.space>
2018-06-25 00:10:55 +1000

    MM-17: Persistent storage

--- src/shared/store/createInitState.js
+++ src/shared/store/createInitState.js
@@ -1,11 +1,37 @@
 // @flow
 import type { DynamicStorage } from '../types/storage.types.js';
+import initStorage from './initStorage';
+import { AsyncStorage } from 'react-native';
 
-export default function createInitState(storage: any) {
+import { setMinerId } from './persistentStorage';
+
+const PATH_STORAGE_REF = 'PATH_STORAGE';
+
+const createInitState = async () => {
+  const storage = JSON.parse(await AsyncStorage.getItem('PATH_STORAGE'));
+
+  const deviceStored = {
+    miner_id: storage.miner_id || ''
+  };
+  const optionsStored = {
+    wallet: storage.wallet || '',
+    wifi_enabled: storage.wifi_enabled || ''
+  };
+
   return {
     job: {},
-    device: {},
+    device: {
+      info: {
+        ...deviceStored
+      }
+    },
     socketClient: {},
-    ...storage
+    options: {
+      userSettings: {
+        ...optionsStored
+      }
+    }
   };
-}
+};
+
+export default createInitState;
